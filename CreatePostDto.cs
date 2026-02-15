83cfea32d75c6611961d838fd9b34f086b5483e4
Vasil1411 <145379847+Vasil1411@users.noreply.github.com>
2026-02-15 19:25:26 +0200

    Delete CreatePostDto.cs

--- CreatePostDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace WebApi.DTOs;
-
-public class CreatePostDto
-{
-    [Required]
-    [MinLength(10)]
-    public string Content { get; set; }
-}
