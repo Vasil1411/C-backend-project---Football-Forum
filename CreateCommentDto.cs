b16eced9ecd7119fd6093e568deb5d9e5922fc59
Vasil1411 <145379847+Vasil1411@users.noreply.github.com>
2026-02-15 19:24:17 +0200

    Delete CreateCommentDto.cs

--- CreateCommentDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace WebApi.DTOs;
-
-public class CreateCommentDto
-{
-    [Required]
-    [MinLength(5)]
-    public string Content { get; set; }
-}
