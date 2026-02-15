0999162e089d451eff8a04b930fa71cad34e59a8
Vasil1411 <145379847+Vasil1411@users.noreply.github.com>
2026-02-15 19:24:58 +0200

    Delete TeamResDto.cs

--- TeamResDto.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApi.DTOs;
-
-public class TeamResponseDto
-    {
-        public int TeamId { get; set; }     
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public string CreatorName { get; set; } 
-    }
